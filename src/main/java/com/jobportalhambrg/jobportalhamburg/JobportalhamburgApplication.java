fdbe3460fe35fbd647ff75485ed5c6fe7facd26b
Maxwell IG <m.imasuen-goertzen@perfect-media-solutions.de>
2024-09-06 06:50:37 +0200

    Set up Spring Boot project

---
+++ src/main/java/com/jobportalhambrg/jobportalhamburg/JobportalhamburgApplication.java
@@ -0,0 +1,13 @@
+package com.jobportalhambrg.jobportalhamburg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobportalhamburgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobportalhamburgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobportalhambrg/jobportalhamburg/JobportalhamburgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobportalhambrg.jobportalhamburg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobportalhamburgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
