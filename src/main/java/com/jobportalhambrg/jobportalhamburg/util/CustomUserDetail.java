98ba2828621be16d6de663e51639c9adbb30ac34
Maxwell IG <m.imasuen-goertzen@perfect-media-solutions.de>
2024-09-18 07:23:15 +0200

    Implemented Custom User Azthentication

---
+++ src/main/java/com/jobportalhambrg/jobportalhamburg/util/CustomUserDetail.java
@@ -0,0 +1,59 @@
+package com.jobportalhambrg.jobportalhamburg.util;
+
+import com.jobportalhambrg.jobportalhamburg.entity.Users;
+import com.jobportalhambrg.jobportalhamburg.entity.UsersType;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetail implements UserDetails {
+
+    private final Users user;
+
+    public CustomUserDetail(Users user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        UsersType usersType = user.getUserTypeId();
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(usersType.getUserTypeName()));
+        return authorities;
+        //return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
